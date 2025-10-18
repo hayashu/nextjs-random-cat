6328488b4aaf0186c036ba78f482d76da37148ab
hayashu <kindsun884.730@gmail.com>
2025-10-18 18:15:02 +0900

    envに関する文章を削除

--- src/app/fetch-image.ts
+++ src/app/fetch-image.ts
@@ -1,17 +1,18 @@
 "use server";
 
-import { CAT_API_KEY } from "./env";
+// import { CAT_API_KEY } from "./env";
 
 type Image = {
   url: string;
 }
 
 export async function fetchImage() : Promise<Image> {
-  const res = await fetch("https://api.thecatapi.com/v1/images/search", {
-    headers: {
-      "x-api-key": CAT_API_KEY
-    }
-  });
+  const res = await fetch("https://api.thecatapi.com/v1/images/search"
+    // , {
+    // headers: {
+    //   "x-api-key": CAT_API_KEY
+    // }  }
+  );
   
   const images : unknown = await res.json();
   console.log("fetchImage: 画像を取得", images);
